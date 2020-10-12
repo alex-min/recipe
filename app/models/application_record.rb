bf2a4bdfb7d0401f94d7d9145e2f4bf1fcef3b58
alex-min <contact@alex-min.fr>
2020-10-12 19:58:00 +0200

    fixing deploy

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,13 @@
-require "select_append.rb"
+module ActiveRecord
+  module QueryMethods
+    def select_append(*fields)
+      if(!select_values.any?)
+        fields.unshift(arel_table[Arel.star])
+      end
+      select(*fields)
+    end
+  end
+end
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
