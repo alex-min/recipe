5f19e2f31ee868b5ab098bdca3b61d2e0763e01a
alex-min <contact@alex-min.fr>
2020-10-13 12:22:44 +0200

    zeitwerk

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
+module ActiveRecord
+    module QueryMethods
+      def select_append(*fields)
+        if(!select_values.any?)
+          fields.unshift(arel_table[Arel.star])
+        end
+        select(*fields)
+      end
+    end
+end
+  
 class Recipe < ApplicationRecord
     has_many :ingredients
 
