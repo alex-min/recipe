7d4637c0f2ecb19b9552626b7b25503680356648
alex-min <contact@alex-min.fr>
2020-10-13 12:21:17 +0200

    zeitwerk

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+module ActiveRecord
+    module QueryMethods
+      def select_append(*fields)
+        if(!select_values.any?)
+          fields.unshift(arel_table[Arel.star])
+        end
+        select(*fields)
+      end
+    end
+end
+  
