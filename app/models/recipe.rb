db19dc42081b0ffc6640711a1e8b86d296f40762
alex-min <contact@alex-min.fr>
2020-10-13 12:25:52 +0200

    fixing build

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,20 +1,8 @@
-module ActiveRecord
-    module QueryMethods
-      def select_append(*fields)
-        if(!select_values.any?)
-          fields.unshift(arel_table[Arel.star])
-        end
-        select(*fields)
-      end
-    end
-end
-  
 class Recipe < ApplicationRecord
     has_many :ingredients
 
     def self.containing_ingredients(ingredients)
         self.joins(:ingredients)
-            .select_append('count(ingredients.name)')
             .where('ingredients.name ilike any (array[?])', ingredients.map {|val| "% #{val} %" })
             .order('count(ingredients.name) desc')
             .group(:id)
